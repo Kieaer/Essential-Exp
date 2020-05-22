6d9efc6297ef9ee45fb76aa2baceb461737ceebb
키에르 <44261958+Kieaer@users.noreply.github.com>
2020-05-22 23:28:47 +0900

    기본 스탯 추가? (시험 안해봄)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,31 @@ import arc.ApplicationListener;
 import arc.Core;
 import arc.util.CommandHandler;
 import mindustry.Vars;
-import mindustry.content.Bullets;
+import mindustry.content.Mechs;
 import mindustry.entities.type.Player;
-import mindustry.game.Team;
+import mindustry.game.Rules;
 import mindustry.gen.Call;
 import mindustry.mod.Mods;
 import mindustry.plugin.Plugin;
+import mindustry.type.Mech;
+import mindustry.type.Weapon;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Random;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import static java.lang.Thread.sleep;
+import static mindustry.Vars.playerGroup;
+import static mindustry.Vars.state;
 
 public class Main extends Plugin {
     public ApplicationListener listener;
+    public Connection conn;
+    public Map<String, Integer> data = new HashMap<>();
+    Thread mainThread;
+    TimerTask tickThread;
+    Timer timer = new Timer();
 
     public Main(){
 
@@ -36,11 +45,30 @@ public class Main extends Plugin {
 
         if(work){
             try {
-                Connection conn = DriverManager.getConnection("jdbc://localhost:9079/player", "", "");
+                conn = DriverManager.getConnection("jdbc://localhost:9079/player", "", "");
+
                 listener = new ApplicationListener() {
                     @Override
                     public void update() {
+                        data.forEach((name, level) -> {
+                            Player p = playerGroup.find(pl -> pl.name.equalsIgnoreCase(name));
+                            if(p != null) {
+                                if(level > 5 && level < 10){
+                                    p.mech.mineSpeed = (p.mech.mineSpeed * 100f) / 7;
+                                    p.mech.health = (p.mech.health * 100f) / 7;
+                                    p.mech.boostSpeed = (p.mech.boostSpeed * 100f) / 7;
+                                    p.mech.maxSpeed = (p.mech.maxSpeed * 100f) / 7;
+                                } else if (level > 11 && level < 20){
 
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void dispose() {
+                        timer.cancel();
+                        mainThread.interrupt();
                     }
                 };
 
@@ -48,13 +76,34 @@ public class Main extends Plugin {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+
+            mainThread = new Thread(() -> {
+
+            });
+
+            tickThread = new TimerTask() {
+                int time = 0;
+
+                @Override
+                public void run() {
+                    if(time == 30){
+                        data.clear();
+                        try(PreparedStatement pstmt = conn.prepareStatement("SELECT * from players WHERE connected=1")){
+                            try(ResultSet rs = pstmt.executeQuery()){
+                                while(rs.next()){
+                                    data.put(rs.getString("name"), rs.getInt("level"));
+                                }
+                            }
+                        } catch (SQLException e){
+                            e.printStackTrace();
+                        }
+                    }
+
+                    time++;
+                }
+            };
+
+            timer.scheduleAtFixedRate(tickThread, 1000, 1000);
         }
-    }
-
-    @Override
-    public void registerClientCommands(CommandHandler handler) {
-        handler.<Player>register("cha", "Test geo", (arg, player) -> {
-
-        });
     }
 }
